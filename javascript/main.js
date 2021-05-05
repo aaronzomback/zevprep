4c17e01b81fd01a11ad93650a8576509cdcdf69e
aaronzomback <aaronzomback@gmail.com>
2021-05-05 11:51:21 +0200

    hamburger updating

--- javascript/main.js
+++ javascript/main.js
@@ -8,4 +8,17 @@ $(document).ready(() => {
   $('#close-form').on("click", () => {
     $('#contact-div').hide();
   })
+
+
+  // hamburger nav
+
+  $('#hamburger-icon').on("click", () => {
+    $('#hamburger-nav').toggle();
+    $('#hamburger-icon').hide();
+  });
+
+  $('#ham-close').on("click", () => {
+    $('#hamburger-nav').hide();
+    $('#hamburger-icon').show();
+  })
 });
